0914b8cd73c35bad141de8e7d313598e88f101bc
phantamanta44 <evandalong@gmail.com>
2020-08-01 15:34:53 -0500

    Resync config after editing the in-game config gui

--- src/main/java/xyz/phanta/psicosts/client/ClientProxy.java
+++ src/main/java/xyz/phanta/psicosts/client/ClientProxy.java
@@ -1,7 +1,9 @@
 package xyz.phanta.psicosts.client;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import xyz.phanta.psicosts.CommonProxy;
+import xyz.phanta.psicosts.client.event.ConfigGuiHandler;
 import xyz.phanta.psicosts.util.MagicCircleRender;
 
 public class ClientProxy extends CommonProxy {
@@ -9,6 +11,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void onPreInit(FMLPreInitializationEvent event) {
         super.onPreInit(event);
+        MinecraftForge.EVENT_BUS.register(new ConfigGuiHandler());
         intManager.initClient();
     }
 
